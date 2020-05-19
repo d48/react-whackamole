0a8d7c719e425544f0470dcd8296df44a44ea0b8
Ryan Regalado <ryanreg48@gmail.com>
2020-05-18 19:41:52 -0700

    feat(App): grid and styling
    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState} from "react";
 import "./styles.css";
 
 export default function App() {
+  let [score, setScore] = useState(0)
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header>
+        <h1>React - Whack a Mole Game</h1>
+        <h2>Score: {score}</h2>
+      </header>
+      
+      <ul id="squares">
+        <li class="square" id="1"></li>
+        <li class="square" id="2"></li>
+        <li class="square" id="3"></li>
+        <li class="square" id="4"></li>
+        <li class="square" id="5"></li>
+        <li class="square" id="6"></li>
+        <li class="square" id="7"></li>
+        <li class="square" id="8"></li>
+        <li class="square" id="9"></li>
+      </ul>
     </div>
   );
 }
