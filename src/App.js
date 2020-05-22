e4f4c007172eed0ae5d32a70146d6abacc606fb4
Ryan Regalado <ryanreg48@gmail.com>
2020-05-22 01:01:25 -0700

    chore(App): cleanup unused method

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 import "./styles.css";
 import { buildSquares } from "./components/Square";
 import { moveMole } from "./mole.js";
