26d5f2935b13c2266ad9446aa457bf7a55f0326c
Ryan Regalado <ryanreg48@gmail.com>
2020-05-22 00:44:13 -0700

    feat(App): add start/stop buttons and setInterval logic

--- src/App.js
+++ src/App.js
@@ -3,23 +3,46 @@ import "./styles.css";
 import { buildSquares } from "./components/Square";
 import { moveMole } from "./mole.js";
 
-export default function App() {
-  let [score, setScore] = useState(0)
-  let [squares, setSquares] = useState(buildSquares(9))
-  const squareEls = useRef();
-  
-  useEffect(() => {    
-    console.log(squareEls.current.children)
+let timerId;
+let squareEls;
+
+function startGame(startHandler) {
+  startHandler(true);
+  timerId = setInterval(() => {
     moveMole(squareEls.current.children);
-  });
- 
+  }, 2500);
+}
+
+function stopGame(startHandler) {
+  clearTimeout(timerId);
+  startHandler(false);
+}
+
+export default function App() {
+  let [score, setScore] = useState(0);
+  let [squares, setSquares] = useState(buildSquares(9));
+  let [gameStart, setGameStart] = useState(false);
+  let [timer, setTimer] = useState(60);
+  squareEls = useRef();
+
   return (
     <div className="App">
       <header>
         <h1>Whack-a-Mole Game</h1>
-        { score ? <h2>Score: {score}</h2> : <h2><button>Start Game</button></h2> }
+        <h2>Score: {score}</h2>
+
+        {gameStart ? (
+          <>
+            <h3>Timer: {timer}</h3>
+            <button onClick={() => stopGame(setGameStart)}>Stop Game</button>
+          </>
+        ) : (
+          <h2>
+            <button onClick={() => startGame(setGameStart)}>Start Game</button>
+          </h2>
+        )}
       </header>
-      
+
       <ul id="squares" ref={squareEls}>
         {squares}
       </ul>
--- src/mole.js
+++ src/mole.js
@@ -1,22 +1,20 @@
-
-const moveMole = (squares) => {  
+const moveMole = squares => {
   for (let square of squares) {
-    console.log(square)
-    removeMole(square)
+    removeMole(square);
   }
-   
-  let randomNum = Math.ceil(Math.random() * 9)
-  addMole(squares[randomNum])
+
+  let randomNum = Math.ceil(Math.random() * 9);
+  addMole(squares[randomNum - 1]);
 };
 
-const addMole = (element) => {
-  element.classList.add('mole')
+const addMole = element => {
+  element && element.classList.add("mole");
   return element;
-}
+};
 
-const removeMole = (element) => {
-  element.classList.remove('mole')
-  return element
-}
+const removeMole = element => {
+  element && element.classList.remove("mole");
+  return element;
+};
 
 export { addMole, moveMole, removeMole };
