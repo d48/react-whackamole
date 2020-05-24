b5d94f21f645b739089948866e0ce909885bbcd3
Ryan Regalado <ryanreg48@gmail.com>
2020-05-24 11:16:24 -0700

    feat(Mole): add test to remove all moles from a list of elements

--- src/mole.spec.js
+++ src/mole.spec.js
@@ -1,4 +1,4 @@
-import { addMole, removeMole } from "./mole"
+import { addMole, removeMole, removeMoles } from "./mole"
 
 describe("mole test suite", () => {
   it("should add a .mole class to an element", () => {
@@ -24,4 +24,23 @@ describe("mole test suite", () => {
     expect(element.className).not.toContain('mole')   
   })
 
+  it("should remove all moles classes on a list of elements", () => {
+    // arrange
+    let mockElement = document.createElement('li')
+    let mockElement2 = document.createElement('li')
+    let mockElement3 = document.createElement('li')
+
+    // act
+    mockElement.classList.add('mole')
+    mockElement2.classList.add('mole')
+    mockElement3.classList.add('mole')
+
+    let mocks = [mockElement, mockElement2, mockElement3]
+    removeMoles(mocks)
+
+    // assert
+    expect(mocks[0].className).not.toContain('mole')
+    expect(mocks[1].className).not.toContain('mole')
+    expect(mocks[2].className).not.toContain('mole')
+  })
 })
